e3cdd011bee75be74060b9773223b5589388ed24
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-07-30 13:42:50 +0530

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -11,55 +11,136 @@
     See changelogs if any update is available.
 """
 
+import asyncio
+import sys
+from os import environ, execle, path, remove
+from typing import Tuple
 from git import Repo
-from telethon.tl.functions.channels import ExportMessageLinkRequest as GetLink
-
-from . import *
-
-ULTPIC = "resources/extras/inline.jpg"
-CL = udB.get("INLINE_PIC")
-if CL:
-    ULTPIC = CL
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 
-@ultroid_cmd(pattern="update$")
-async def _(e):
-    xx = await eor(e, "`Checking for updates...`")
-    m = await updater()
-    branch = (Repo.init()).active_branch
-    if m:
-        x = await asst.send_file(
-            int(udB.get("LOG_CHANNEL")),
-            ULTPIC,
-            caption="• **Update Available** •",
-            force_document=False,
-            buttons=Button.inline("Changelogs", data="changes"),
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME 
+HEROKU_API_KEY = Var.HEROKU_API
+
+
+requirements_path = path.join(
+    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
+)
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(*args,
+                                                   stdout=asyncio.subprocess.PIPE,
+                                                   stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    return (stdout.decode('utf-8', 'replace').strip(),
+            stderr.decode('utf-8', 'replace').strip(),
+            process.returncode,
+            process.pid)
+
+
+async def update_requirements():
+    reqs = str(requirements_path)
+    try:
+        process = await asyncio.create_subprocess_shell(
+            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
-        if not e.client._bot:
-            Link = (await e.client(GetLink(x.chat_id, x.id))).link
+        await process.communicate()
+        return process.returncode
+    except Exception as e:
+        return repr(e)
+
+
+async def deploy(event, repo, ups_rem, ac_br, txt):
+    if HEROKU_API_KEY is not None:
+        import heroku3
+
+        heroku = heroku3.from_key(HEROKU_API_KEY)
+        heroku_app = None
+        heroku_applications = heroku.apps()
+        if HEROKU_APP_NAME is None:
+            await event.edit(
+                "`Please set up the` **HEROKU_APP_NAME** `Var`"
+                " to be able to deploy your userbot...`"
+            )
+            repo.__del__()
+            return
+        for app in heroku_applications:
+            if app.name == HEROKU_APP_NAME:
+                heroku_app = app
+                break
+        if heroku_app is None:
+            await event.edit(
+                f"{txt}\n" "`Invalid Heroku credentials for deploying userbot dyno.`"
+            )
+            return repo.__del__()
+        await event.edit(
+            "`Userbot dyno build in progress, please wait until the process finishes it usually takes 4 to 5 minutes .`"
+        )
+        ups_rem.fetch(ac_br)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        heroku_git_url = heroku_app.git_url.replace(
+            "https://", "https://api:" + HEROKU_API_KEY + "@"
+        )
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(heroku_git_url)
         else:
-            Link = f"https://t.me/c/{x.chat.id}/{x.id}"
-        await xx.edit(
-            f'<strong><a href="{Link}">[ChangeLogs]</a></strong>',
-            parse_mode="html",
-            link_preview=False,
-        )
+            remote = repo.create_remote("heroku", heroku_git_url)
+        try:
+            remote.push(refspec="HEAD:refs/heads/main", force=True)
+        except Exception as error:
+            await event.edit(f"{txt}\n`Here is the error log:\n{error}`")
+            return repo.__del__()
+        build = app.builds(order_by="created_at", sort="desc")[0]
+        if build.status == "failed":
+            await event.edit(
+                "`Build failed!\n" "Cancelled or there were some errors...`"
+            )
+            await asyncio.sleep(5)
+            return await event.delete()
+        await event.edit("`Successfully deployed!\n" "Restarting, please wait...`")
     else:
-        await xx.edit(
-            f'<code>Your BOT is </code><strong>up-to-date</strong><code> with </code><strong><a href="https://github.com/aviskumar/Ultroid-Sudo/tree/{branch}">[{branch}]</a></strong>',
-            parse_mode="html",
-            link_preview=False,
-        )
+        await event.edit("`Please set up`  **HEROKU_API**  ` Var...`")
+    return
 
 
-@callback("updtavail")
-@owner
-async def updava(event):
-    await event.delete()
-    await asst.send_file(
-        int(udB.get("LOG_CHANNEL")),
-        ULTPIC,
-        caption="• **Update Available** •",
-        force_document=False,
-        buttons=Button.inline("Changelogs", data="changes"),
-    )
+@ultroid_cmd(pattern=r"update$")
+
+async def upstream(event):
+    event = await eor(event, "`Pulling the main repo wait a sec ....`")
+    off_repo = "https://github.com/aviskumar/ultroid-sudo"
+    cmd = f"rm -rf .git"
+    try:
+        await runcmd(cmd)
+    except BaseException:
+        pass
+    try:
+        txt = "`Oops.. Updater cannot continue due to "
+        txt += "some problems occured`\n\n**LOGTRACE:**\n"
+        repo = Repo()
+    except NoSuchPathError as error:
+        await event.edit(f"{txt}\n`directory {error} is not found`")
+        return repo.__del__()
+    except GitCommandError as error:
+        await event.edit(f"{txt}\n`Early failure! {error}`")
+        return repo.__del__()
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        repo.create_head("main", origin.refs.main)
+        repo.heads.main.set_tracking_branch(origin.refs.main)
+        repo.heads.main.checkout(True)
+    try:
+        repo.create_remote("upstream", off_repo)
+    except BaseException:
+        pass
+    ac_br = "main"
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
+    await event.edit("`Deploying userbot, please wait....`")
+    await deploy(event, repo, ups_rem, ac_br, txt)
